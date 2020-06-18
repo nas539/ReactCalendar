efa0397e070398c1ea63bfc9364a5231c9b4ee25
Nicholas Sharp <naas8905@gmail.com>
2020-06-18 09:18:33 -0700

    Adding styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function footer(props) {
     return (
-        <div>
+        <div className="footer-wrapper">
             <h3>{props.year}</h3>
         </div>
     )
--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function header(props) {
     return (
-        <div>
+        <div className="header-wrapper">
             <button>Previous</button>
             <h2>{props.month}</h2>
             <button>Next</button>
