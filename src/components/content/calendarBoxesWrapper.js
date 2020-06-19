2bed1258e6a43cff79ecf26972c71b09eed05857
Nicholas Sharp <naas8905@gmail.com>
2020-06-19 09:36:31 -0700

    Adding days container

---
+++ src/components/content/calendarBoxesWrapper.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import CalendarBox from "./calendarBox"
+
+export default function calendarBoxesWrapper(props) {
+    const renderBoxes = () => {
+        const boxesContainer = [];
+
+        for (let i = 1; i <= props.startDay; i++) {
+            let date = props.daysInPreviousMonth - (props.startDay - i)
+
+            boxesContainer.push(
+                <CalendarBox key={`P${i}`} date={date} />
+            )
+        }
+
+        for (let i = 1; i <= props.daysInMonth; i++) {
+            boxesContainer.push(
+                <CalendarBox key={i} date={i} />
+            )
+        }
+
+        for (let i = 0; i <= (42 - (props.startDay + props.daysInPreviousMonth)); i++) {
+            boxesContainer.push(
+                <CalendarBox key={`N${i}`} date={i} />
+            )
+        }
+        return boxesContainer
+    }
+
+    return (
+        <div className="calendar-boxes-wrapper">
+            {renderBoxes()}
+        </div>
+    )
+}
--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import DaysWrapper from "./days-wrapper.js";
+import CalendarBoxesWrapper from "./calendarBoxesWrapper"
 
 export default function contentWrapper(props) {
     return (
         <div className="content-wrapper">
             <DaysWrapper />
+            <CalendarBoxesWrapper 
+                daysInMonth={props.daysInMonth}
+                daysInPreviousMonth={props.daysInPreviousMonth}
+                startDay={props.startDay}/>
         </div>
     )
 }
